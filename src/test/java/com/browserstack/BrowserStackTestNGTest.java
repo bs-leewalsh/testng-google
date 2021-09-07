7dea559e6950f58328f5a36c79f1a8c02a2a1fe2
Lee Walsh <lee.w@browserstack.com>
2021-09-07 17:35:00 +0100

    TestNG-Google Upload

--- src/test/java/com/browserstack/BrowserStackTestNGTest.java
+++ src/test/java/com/browserstack/BrowserStackTestNGTest.java
@@ -1,15 +1,10 @@
 package com.browserstack;
 
-import java.io.FileReader;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 import com.browserstack.local.Local;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -21,51 +16,26 @@ public class BrowserStackTestNGTest {
     private Local l;
 
     @BeforeMethod(alwaysRun = true)
-    @org.testng.annotations.Parameters(value = { "config", "environment" })
-    @SuppressWarnings("unchecked")
-    public void setUp(String config_file, String environment) throws Exception {
-        JSONParser parser = new JSONParser();
-        JSONObject config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
-        JSONObject envs = (JSONObject) config.get("environments");
+    public void setUp() throws Exception {
+    	
+    	String username = System.getenv("BROWSERSTACK_USERNAME");
+    	String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+    	String buildName = System.getenv("BROWSERSTACK_BUILD_NAME");
+    	String browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
+    	String browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+    	DesiredCapabilities capabilities = new DesiredCapabilities();
+    	capabilities.setCapability("os", "Windows");
+    	capabilities.setCapability("os_version", "10");
+    	capabilities.setCapability("browser", "chrome");
+    	capabilities.setCapability("browser_version", "latest");
+    	capabilities.setCapability("name", "BStack-[Java] Sample Test"); // test buildName
+    	capabilities.setCapability("build", buildName); // CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
+    	capabilities.setCapability("browserstack.local", browserstackLocal);
+    	capabilities.setCapability("browserstack.localIdentifier", browserstackLocalIdentifier);
+    	driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"), capabilities);
 
-        Map<String, String> envCapabilities = (Map<String, String>) envs.get(environment);
-        Iterator it = envCapabilities.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry) it.next();
-            capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
-        }
-
-        Map<String, String> commonCapabilities = (Map<String, String>) config.get("capabilities");
-        it = commonCapabilities.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry) it.next();
-            if (capabilities.getCapability(pair.getKey().toString()) == null) {
-                capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
-            }
-        }
-
-        String username = System.getenv("BROWSERSTACK_USERNAME");
-        if (username == null) {
-            username = (String) config.get("user");
-        }
-
-        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
-        if (accessKey == null) {
-            accessKey = (String) config.get("key");
-        }
-
-        if (capabilities.getCapability("browserstack.local") != null
-                && capabilities.getCapability("browserstack.local") == "true") {
-            l = new Local();
-            Map<String, String> options = new HashMap<String, String>();
-            options.put("key", accessKey);
-            l.start(options);
-        }
-
-        driver = new RemoteWebDriver(
-                new URL("http://" + username + ":" + accessKey + "@" + config.get("server") + "/wd/hub"), capabilities);
+        
     }
 
     @AfterMethod(alwaysRun = true)
--- src/test/java/com/browserstack/SingleTest.java
+++ src/test/java/com/browserstack/SingleTest.java
@@ -1,21 +1,46 @@
 package com.browserstack;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SingleTest extends BrowserStackTestNGTest {
+	
 
-    @Test
-    public void test() throws Exception {
-        driver.get("https://www.google.com/ncr");
-        WebElement element = driver.findElement(By.name("q"));
-        element.sendKeys("BrowserStack");
-        element.submit();
-        Thread.sleep(5000);
+	@Test(priority=0)
+	public void GoogleSearch() {
+		driver.get("https://www.google.com/");
+		driver.findElement(By.xpath("//button[@id='L2AGLb']")).click();
+		driver.findElement(By.name("q")).sendKeys("BrowserStack");
+		driver.findElement(By.name("btnK")).click();
 
-        Assert.assertTrue(driver.getTitle().matches("(?i)BrowserStack - Google Search"));
-    }
+	}
+
+	@Test(priority=1)
+	public void SearchResults() {
+		driver.get("https://www.google.com/");
+		driver.findElement(By.xpath("//button[@id='L2AGLb']")).click();
+		driver.findElement(By.name("q")).sendKeys("BrowserStack");
+		driver.findElement(By.name("btnK")).click();
+		List<WebElement> ResultsReturned = driver
+		.findElements(By.xpath("//div[@class='yuRUbf']"));
+		System.out.println("First Page Results Returned:" + ResultsReturned.size());
+	}
+	
+	@Test(priority=2)
+	public void GoogleHelp() {
+		driver.get("https://www.google.com/");
+		driver.findElement(By.xpath("//button[@id='L2AGLb']")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'Help')]")).click();
+		System.out.println(driver.getTitle());
+	}
+
+	@Test(priority=3)
+	public void SearchAdvertising() {
+		driver.get("https://www.google.com/");
+		driver.findElement(By.xpath("//button[@id='L2AGLb']")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'Advertising')]")).click();
+	}
 }
